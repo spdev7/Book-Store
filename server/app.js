9466ea174e59ea51833b1cd10c64de49d5c35810
spdev7 <sauravprakash7@outlook.com>
2019-02-07 21:55:31 +0530

    finished

--- server/app.js
+++ server/app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-const app = express();
+const cors = require('cors');
 
+
+
+const app = express();
 const PORT = process.env.PORT || 4000;
+
+//allow cross-origin requests
+app.use(cors());
+
 //Connection to database
-mongoose.connect('mongodb://helll:saurav123@ds123675.mlab.com:23675/graphqlsp');
-mongoose.connection.once('open',()=>{
-    console.log('Connection to database');
-});
+// mongoose.connect('mongodb://helll:saurav123@ds123675.mlab.com:23675/graphqlsp');
+// mongoose.connection.once('open',()=>{
+//     console.log('Connection to database');
+// });
+
+mongoose.connect('mongodb://hello:saurav123@ds123675.mlab.com:23675/graphqlsp' , { useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 //Middleware to interact with graphQL
 app.use('/graphql',graphqlHTTP({
    schema,
