3932f5612797747ca911989d7fdd30f558805380
spdev7 <sauravprakash7@outlook.com>
2019-02-07 09:23:04 +0530

    Now setting up Database on mlab

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-
+//Connection to database
+mongoose.connect('mongodb://helll:saurav123@ds123675.mlab.com:23675/graphqlsp');
+mongoose.connection.once('open',()=>{
+    console.log('Connection to database');
+});
 //Middleware to interact with graphQL
 app.use('/graphql',graphqlHTTP({
-   schema
-}))
+   schema,
+   graphiql:true
+}));
 
 app.listen(PORT,()=>{
     console.log('Server up and running');
